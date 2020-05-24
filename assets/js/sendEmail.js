b9a968e95f1f9b140f8bc84cc51006f3cef7c1be
ss00831 <songhei_831.lee@hotmail.com>
2020-05-24 21:09:24 +0000

    code formatting and fixed footer margin-top (remove wrap div)

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,23 +1,23 @@
 (function () {
-  emailjs.init("user_GSvvZT0J6KvBUXPodOVCh");
+    emailjs.init("user_GSvvZT0J6KvBUXPodOVCh");
 })();
 
 function sendMail(contactForm) {
-  emailjs
-    .send("gmail", "template_3nbA1YQC", {
-      type_of_request: contactForm.subject.value,
-      from_name: contactForm.name.value,
-      from_email: contactForm.emailaddress.value,
-      project_request: contactForm.projectsummary.value,
-    })
-    .then(
-      function (response) {
-        alert("The email has been sent successfully.");
-        $("#mail-form").get(0).reset();
-      },
-      function (error) {
-        alert("Try again.");
-      }
-    );
-  return false; // To block from loading a new page
+    emailjs
+        .send("gmail", "template_3nbA1YQC", {
+            type_of_request: contactForm.subject.value,
+            from_name: contactForm.name.value,
+            from_email: contactForm.emailaddress.value,
+            project_request: contactForm.projectsummary.value,
+        })
+        .then(
+            function (response) {
+                alert("The email has been sent successfully.");
+                $("#mail-form").get(0).reset();
+            },
+            function (error) {
+                alert("Try again.");
+            }
+        );
+    return false; // To block from loading a new page
 }
